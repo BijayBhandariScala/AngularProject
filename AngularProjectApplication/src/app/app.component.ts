71ff75bf410605a961ec9bcd0e5829e2352eb54a
bijay <bijay.bhandari007@gmail.com>
2020-08-17 15:12:20 -0400

    Initial Commit Angular project setup.

---
+++ AngularProjectApplication/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularProjectApplication';
+  sub_title='Practise done by Bijay B. @2020'
+}
